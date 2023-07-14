581ea2815d41fadcb379a8ab706d5547bbb5f3c9
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-07-14 16:40:42 +0800

    Update 修改遊戲機制並新增Ball腳本

---
+++ Assets/script/BallController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	[SerializeField, Header("移動速度"), Range(0, 10)]
+	float speed = 3f;
+	[SerializeField, Header("移動方向")]
+	Vector2 direction = Vector2.zero;
+
+	private Rigidbody2D rig2D;
+	private float random_X;
+	private float random_Y;
+	[Tooltip("是否碰到牆")]
+	private bool hitWall = false;   // 是否碰到牆
+
+	private void Awake()
+	{
+		rig2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		random_X = Random.Range(0, 180);
+		random_Y = Random.Range(0, 180);
+	}
+
+	private void Update()
+	{
+		BallMove();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		int randomValue;
+
+		if (collision.gameObject.name.Contains("air wall") || collision.gameObject.name.Contains("ball"))
+		{
+			hitWall = true;
+			
+			if (Random.value < 0.5)
+			{
+				randomValue = -1;
+			}
+			else
+			{
+				randomValue = 1;
+			}
+			random_X = randomValue * Random.Range(0, 180);
+			random_Y = randomValue * Random.Range(0, 180);
+		}
+	}
+
+	/// <summary>
+	/// 球移動
+	/// </summary>
+	void BallMove()
+	{
+		direction = new Vector2(random_X, random_Y);
+		rig2D.velocity = direction * speed * Time.deltaTime;
+		// rig2D.AddForce(direction * speed * Time.deltaTime, ForceMode2D.Force);
+		/*
+		if (hitWall)
+		{
+			float randomValue = Random.Range(1f, 6f);
+
+			random_X = -1 * random_X;
+			random_Y = -1 * random_Y;
+
+			// Debug.Log($"<color=yellow>{randomValue}</color>");
+		}
+		*/
+		// Debug.Log($"<color=#900500>{hitWall}</color>");
+	}
+}
--- Assets/script/Play.cs
+++ Assets/script/Play.cs
@@ -1,35 +1,91 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Play : MonoBehaviour
 {
-    [Header("隨機球")]
-    [SerializeField] GameObject[] ballArray = new GameObject[0];
-    [Header("最小X軸")]
-    [SerializeField] float xMin = 0f;
-    [Header("最大X軸")]
-    [SerializeField] float xMax = 0f;
-    [Header("最小Y軸")]
-    [SerializeField] float yMin = 0f;
-    [Header("最大Y軸")]
-    [SerializeField] float yMax = 0f;
+	[SerializeField, Header("隨機球物件")]
+	GameObject ballObject = null;
+	[SerializeField, Header("生成數量"), Range(1, 100)]
+	int ballCount = 1;
+	[SerializeField, Header("球的顏色")]
+	SpriteRenderer[] colorArray;
+	[SerializeField, Header("最小X軸範圍")]
+	float xMin = 0f;
+	[SerializeField, Header("最大X軸範圍")]
+	float xMax = 0f;
+	[SerializeField, Header("最小Y軸範圍")]
+	float yMin = 0f;
+	[SerializeField, Header("最大Y軸範圍")]
+	float yMax = 0f;
+	[SerializeField, Header("中心偏移量")]
+	Vector3 offset;
+	[SerializeField, Header("繪製X軸範圍")]
+	float offset_X;
+	[SerializeField, Header("繪製Y軸範圍")]
+	float offset_Y;
 
-    private void Start()
-    {
-        CreateBall();
+	List<GameObject> ballList = new List<GameObject>();
 
-    }
+	private void Awake()
+	{
+		// 指定陣列大小
+		colorArray = new SpriteRenderer[ballCount];
+		CreateBall();
+	}
 
-    // 生成隨機球
-    private void CreateBall()
-    {
-        float xRan = Random.Range(xMin, xMax);                      // 設置隨機X軸座標介於最大與最小之間
-        float yRan = Random.Range(yMin, yMax);                      // 設置隨機Y軸座標介於最大與最小之間
-                
-        for (int i = 0; i < ballArray.Length; i++)
-        {
-            int noRan = Random.Range(0, ballArray.Length);              // 隨機取得編號
-            Vector3 pos = new Vector3(xRan, yRan, 0f);                   // 設置隨機座標
-            Instantiate(ballArray[noRan], pos, Quaternion.identity);    // 生成隨機位置
-        }
-    }
+	private void Start()
+	{
+		// 取得所有特定物件的指定組件
+		GetComponentArrayByObject("ball");
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawCube(transform.position + offset, new Vector3(offset_X, offset_Y, 0f));
+	}
+
+	/// <summary>
+	/// 生成隨機球
+	/// </summary>
+	private void CreateBall()
+	{
+		float xRan = Random.Range(xMin, xMax);                  // 設置隨機X軸座標介於最大與最小之間
+		float yRan = Random.Range(yMin, yMax);                  // 設置隨機Y軸座標介於最大與最小之間
+
+		for (int i = 0; i < ballCount; i++)
+		{
+			// int noRan = Random.Range(0, ballCount);          // 隨機取得編號
+			Vector2 pos = new Vector2(xRan, yRan);              // 設置隨機座標
+																// GameObject tempBall = Instantiate(ballObject);
+			Instantiate(ballObject, pos, Quaternion.identity);  // 生成隨機位置
+		}
+		// Debug.Log($"<color=blue>{ballCount}</color>");
+	}
+
+	/// <summary>
+	/// 隨機給予球顏色
+	/// </summary>
+	void RandomColor()
+	{
+		for (int i = 0; i < colorArray.Length; i++)
+		{
+			float r = Random.Range(0, 255);
+			float g = Random.Range(0, 255);
+			float b = Random.Range(0, 255);
+			colorArray[i].color = new Color(r, g, b);
+		}
+	}
+
+	/// <summary>
+	/// 取得所有特定物件的指定組件
+	/// </summary>
+	/// <param name="name">物件名稱</param>
+	void GetComponentArrayByObject(string name)
+	{
+		for (int i = 0; i < ballCount; i++)
+		{
+			colorArray[i] = GameObject.Find(name).GetComponent<SpriteRenderer>();
+		}
+	}
 }
